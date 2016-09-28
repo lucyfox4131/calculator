2419b2db1d4bf1169d8a004fbef4e48ef3fa0cc7
Lucy Fox <lucyreneefox@gmail.com>
2016-09-28 13:17:39 -0600

    Making small changes to play around with app

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Calculator">
+          <div className="Equation-bar">
+            <h3>Values of the calculator will go in here</h3>
+          </div>
+        </div>
       </div>
     );
   }
